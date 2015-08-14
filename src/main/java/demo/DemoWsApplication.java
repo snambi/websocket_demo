2889cca8902cda084c634da3d13063c77cdf7d46
snambi <snambi@gmail.com>
2015-08-14 15:36:01 -0700

    adding the readme file

--- src/main/java/demo/DemoWsApplication.java
+++ src/main/java/demo/DemoWsApplication.java
@@ -1,12 +1,35 @@
 package demo;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class DemoWsApplication {
 
     public static void main(String[] args) {
+    	
+    
         SpringApplication.run( DemoWsApplication.class, args);
+    }
+    
+    @Bean
+    public ServletContextAware endpointExporterInitializer(final ApplicationContext applicationContext) {
+    	
+    	ServletContextAware sca = new ServletContextAware() {
+            @Override
+            public void setServletContext(ServletContext servletContext) {
+                ServerEndpointExporter exporter = new ServerEndpointExporter();
+                exporter.setApplicationContext(applicationContext);
+                exporter.afterPropertiesSet();
+            }
+        };
+        
+        return sca;
     }
 }
---
+++ src/main/java/demo/README_INIT.md
@@ -0,0 +1,6 @@
+
+TomcatEmbeddedServletContainerFactory : Creates An Instance of Tomcat programmatically
+
+TomcatEmbeddedServletContainerFactory.configureContext() : Method configures the SCIs that needed to started by Tomcat
+
+EmbeddedServletContainerAutoConfiguration creates an instance of TomcatEmbeddedServletContainerFactory
